a3eef11e231267380042d945044f43b640ae207d
Ameya Jadhav <jadhavameyak@gmail.com>
2024-09-15 07:15:13 -0400

    last changes

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -4,67 +4,74 @@ import { useMap, Navigation } from '@mappedin/react-sdk';
 import Mappedin from '@mappedin/react-sdk';
 import Groq from 'groq-sdk';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 interface CoordinatesProps {
   start: Mappedin.Coordinate;
   end: any;
   accessibleToggleValue: boolean;
 }
 
+const groq = new Groq({
+  apiKey: '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',
+  dangerouslyAllowBrowser: true,
+});
+
 export const getGroqChatCompletion = async (
   directionsJson: Mappedin.Directions | undefined
 ) => {
   return groq.chat.completions.create({
     messages: [
-      // Set an optional system message. This sets the behavior of the
-      // assistant and can be used to provide specific instructions for
-      // how it should behave throughout the conversation.
       {
         role: 'system',
         content:
-          'You exist to convert a json object of directions that let a person navigate an indoor building. You will be given a json object in a string format, and you will need to covert it to a short, concise paragraph that can be read out loud to a user. These are verbal instructions like Google Maps has, so do not include any other words besides the instructions.',
+          'You exist to convert a json object of directions that let a person navigate an indoor building. You will be given a json object in a string format, and you will need to convert it to a short, concise paragraph that can be read out loud to a user. These should be verbal instructions like Google Maps has, so do not include any extra words like heres the result or anything. I want these instructions to be as clear as possible.',
       },
-      // Set a user message for the assistant to respond to.
       {
         role: 'user',
         content: 'Here is the json object: ' + JSON.stringify(directionsJson),
       },
     ],
-
-    // The language model which will generate the completion.
     model: 'llama3-70b-8192',
-
-    //
-    // Optional parameters
-    //
-
-    // Controls randomness: lowering results in less random completions.
-    // As the temperature approaches zero, the model will become deterministic
-    // and repetitive.
     temperature: 0.5,
-
-    // The maximum number of tokens to generate. Requests can use up to
-    // 2048 tokens shared between prompt and completion.
     max_tokens: 1024,
-
-    // Controls diversity via nucleus sampling: 0.5 means half of all
-    // likelihood-weighted options are considered.
     top_p: 1,
-
-    // A stop sequence is a predefined or user-specified text string that
-    // signals an AI to stop generating content, ensuring its responses
-    // remain focused and concise. Examples include punctuation marks and
-    // markers like "[end]".
     stop: null,
-
-    // If set, partial message deltas will be sent.
     stream: false,
   });
 };
 
-const groq = new Groq({
-  apiKey: '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',
-  dangerouslyAllowBrowser: true,
-});
+async function TTS(text: string) {
+  try {
+    const response = await axios.post(
+      'https://api.v7.unrealspeech.com/stream',
+      {
+        Text: text,
+        VoiceId: 'Will', // You can change this to Scarlett, Liv, Will, or Amy
+        Bitrate: '192k',
+        Speed: '0',
+        Pitch: '1.27',
+        Codec: 'libmp3lame',
+      },
+      {
+        headers: {
+          Authorization: `Bearer eTqIM2sw8B50N5k9RhXHLQ5wckReM04tstSbT5UwWfKKPeBabcoLVp`,
+          'Content-Type': 'application/json',
+        },
+        responseType: 'arraybuffer',
+      }
+    );
+
+    const audioContext = new window.AudioContext();
+    const audioBuffer = await audioContext.decodeAudioData(response.data);
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+    source.start(0);
+  } catch (error) {
+    console.error('Error using Unreal Speech API:', error);
+  }
+}
 
 export default function NavigateBetweenTwoCoordinates({
   start,
@@ -73,56 +80,56 @@ export default function NavigateBetweenTwoCoordinates({
 }: CoordinatesProps) {
   const { mapView, mapData } = useMap();
 
+  const [directionsJson, setDirectionsJson] = useState(null);
   const [textDirections, setTextDirections] = useState('');
 
   useEffect(() => {
     console.log('textDirections', textDirections);
   }, [textDirections]);
 
-  let directionsJson: any = null;
+  useEffect(() => {
+    if (end) {
+      console.log('end', end);
+      if (end.type === 'space') {
+        const space = mapData.getById(end.type, end.spaceId);
+        if (!space) return;
+        console.log('space', space);
+        const directions = mapView.getDirections(start, space, {
+          accessible: accessibleToggleValue,
+        });
 
-  async function groqCompletion(
-    directionsJson: Mappedin.Directions | undefined
-  ) {
-    const chatCompletion = await getGroqChatCompletion(directionsJson);
-    return chatCompletion.choices[0]?.message?.content || '';
-  }
+        const instructions = directions?.instructions;
+        console.log('directionsJson', instructions);
+        setDirectionsJson(instructions);
+      } else if (end.type === 'object') {
+        const [spread, depth] = end.objectLocation;
+        const object = Mappedin.Coordinate(spread);
+        const directions = mapView.getDirections(start, object, {
+          accessible: accessibleToggleValue,
+        });
 
-  if (end) {
-    console.log('end', end);
-    if (end.type == 'space') {
-      const space = mapData.getById(end.type, end.spaceId);
-      if (!space) return null;
-      console.log('space', space);
-      const directions = mapView.getDirections(start, space, {
-        accessible: accessibleToggleValue,
-      });
-
-      // do groq on directions to get the path
-      // text to speech on directions
-
-      directionsJson = directions?.instructions;
-      console.log('directionsJson', directionsJson);
-      const textual_directions = groqCompletion(directionsJson).then((res) => {
-        console.log('res', res);
-        setTextDirections(res);
-      });
-
-      console.log('textual_directions', textual_directions);
-    } else if (end.type == 'object') {
-      const spread = end.objectLocation[0];
-      const depth = end.objectLocation[1];
-
-      const object = Mappedin.Coordinate(end.objectLocation[0]);
-      const directions = mapView.getDirections(start, object, {
-        accessible: accessibleToggleValue,
-      });
-
-      directionsJson = directions;
+        const instructions = directions?.instructions;
+        setDirectionsJson(instructions);
+      }
     }
-  }
+  }, [end, start, accessibleToggleValue, mapView, mapData]);
 
-  console.log(directionsJson);
+  useEffect(() => {
+    if (directionsJson) {
+      const fetchGroqCompletion = async () => {
+        try {
+          const chatCompletion = await getGroqChatCompletion(directionsJson);
+          const res = chatCompletion.choices[0]?.message?.content || '';
+          console.log('res', res);
+          TTS(res);
+          setTextDirections(res);
+        } catch (error) {
+          console.error('Error fetching Groq completion:', error);
+        }
+      };
+      fetchGroqCompletion();
+    }
+  }, [directionsJson]);
 
   return directionsJson ? <Navigation directions={directionsJson} /> : null;
 }
